1bb4d8aec2f7fa82d8fb37eece8735bc0db2e8a2
David Morales <david.morales@connexta.com>
2021-05-29 15:47:49 -0700

    Initial hello world command line test

---
+++ src/main/java/com/david/records/RecordsCommand.java
@@ -0,0 +1,8 @@
+package com.david.records;
+
+public class RecordsCommand {
+  public static void main(String args[]) {
+    System.out.println("Hello world!");
+    System.out.println(String.format("Number of args: %d", args.length));
+  }
+}
