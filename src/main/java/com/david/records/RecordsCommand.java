9e41403b2c54f0dc2afa4f07984ec8fd04f229e3
David Morales <david.morales@connexta.com>
2021-05-30 17:49:43 -0700

    Adds ability to ingest a comma-delimited records file

--- src/main/java/com/david/records/RecordsCommand.java
+++ src/main/java/com/david/records/RecordsCommand.java
@@ -5,10 +5,29 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 import org.jetbrains.annotations.Nullable;
 
 public class RecordsCommand {
+
+  /* This pattern matches a string with the following form:
+   Minelli|Alister|aminelli2@webs.com|Violet|6/24/1990
+
+   The first [^|]+ matches any string up to the first pipe character,
+   then this is repeated 4 more times for the remaining strings separated
+   by each comma character.
+  */
+  private static final Pattern PIPE_DELIMITED_PATTERN = Pattern.compile("^[^|]+(\\|[^|]+){4}$");
+
+  /* This pattern matches a string with the following form:
+   Minelli,Alister,aminelli2@webs.com,Violet,6/24/1990
+
+   The first [^,]+ matches any string up to the first comma character,
+   then this is repeated 4 more times for the remaining strings separated
+   by each comma character.
+  */
+  private static final Pattern COMMA_DELIMITED_PATTERN = Pattern.compile("^[^,]+(,[^,]+){4}$");
 
   public static void main(String args[]) {
 
@@ -30,7 +49,13 @@ public class RecordsCommand {
 
     try (Stream<String> lines = Files.lines(filepath, StandardCharsets.US_ASCII)) {
       lines.forEach(line -> {
-        Record newRecord = parseRecord(line);
+        String delimiterRegex = " ";
+        if (PIPE_DELIMITED_PATTERN.matcher(line).find()) {
+          delimiterRegex = "\\|";
+        } else if (COMMA_DELIMITED_PATTERN.matcher(line).find()) {
+          delimiterRegex = ",";
+        }
+        Record newRecord = parseRecord(line, delimiterRegex);
         if (newRecord == null) {
           System.out.println(String.format("The following record did not have the 5 required fields and was ignored: %s", line));
           return;
@@ -60,8 +85,8 @@ public class RecordsCommand {
   }
 
   @Nullable
-  private static Record parseRecord(String line) {
-    String[] parts = line.split("\\|");
+  private static Record parseRecord(String line, String delimeterRegex) {
+    String[] parts = line.split(delimeterRegex);
     if (parts.length != 5) {
       System.out.println(String.format("parts: %d", parts.length));
       return null;
