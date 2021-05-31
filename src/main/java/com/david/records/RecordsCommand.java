4edda7d540066cc9f3d3d92ee3fa99b04c5f2f96
David Morales <david.morales@connexta.com>
2021-05-30 18:51:11 -0700

    Improves records table readability

--- src/main/java/com/david/records/RecordsCommand.java
+++ src/main/java/com/david/records/RecordsCommand.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 import org.jetbrains.annotations.Nullable;
@@ -59,19 +60,13 @@ public class RecordsCommand {
     }
 
     System.out.println("OUTPUT 1: Sorted by ascending favorite color then last name");
-    System.out.println("First Name\tLast Name\tEmail\tFavorite Color\tDate of Birth");
-    recordsService.getAllRecordsByFavoriteColorThenByLastNameAscending().forEach(r -> System.out.println(print(r)));
-    System.out.println();
+    System.out.println(printTable(recordsService.getAllRecordsByFavoriteColorThenByLastNameAscending()));
 
     System.out.println("OUTPUT 2: Sorted by ascending date of birth");
-    System.out.println("First Name\tLast Name\tEmail\tFavorite Color\tDate of Birth");
-    recordsService.getAllRecordsByDateOfBirthAscending().forEach(r -> System.out.println(print(r)));
-    System.out.println();
+    System.out.println(printTable(recordsService.getAllRecordsByDateOfBirthAscending()));
 
     System.out.println("OUTPUT 3: Sorted by descending last name");
-    System.out.println("First Name\tLast Name\tEmail\tFavorite Color\tDate of Birth");
-    recordsService.getAllRecordsByLastNameDescending().forEach(r -> System.out.println(print(r)));
-    System.out.println();
+    System.out.println(printTable(recordsService.getAllRecordsByLastNameDescending()));
   }
 
   private static void ingest(RecordsService recordsService, Path filepath) throws IOException{
@@ -104,8 +99,79 @@ public class RecordsCommand {
     return new Record(parts[0], parts[1], parts[2], parts[3], parts[4]);
   }
 
-  private static String print(Record record) {
-    return String.format("%s\t%s\t%s\t%s\t%s", record.getFirstName(), record.getLastName(), record.getEmail(), record.getFavoriteColor(), record.getDateOfBirthAsString());
+  private static String printTable(List<Record> records) {
+    String lastNameLabel = "LAST NAME";
+    String firstNameLabel = "FIRST NAME";
+    String emailLabel = "EMAIL";
+    String favoriteColorLabel = "FAVORITE COLOR";
+    String dateOfBirthLabel = "DATE OF BIRTH";
+    int minLastNameSize = lastNameLabel.length();
+    int minFirstNameSize = firstNameLabel.length();
+    int minEmailSize = emailLabel.length();
+    int minFavoriteColorSize = favoriteColorLabel.length();
+    int minDateOfBirthSize = dateOfBirthLabel.length();
+
+    for (Record record : records) {
+      if (record.getLastName().length() > minLastNameSize) {
+        minLastNameSize = record.getLastName().length();
+      }
+      if (record.getFirstName().length() > minFirstNameSize) {
+        minFirstNameSize = record.getFirstName().length();
+      }
+      if (record.getEmail().length() > minEmailSize) {
+        minEmailSize = record.getEmail().length();
+      }
+      if (record.getFavoriteColor().length() > minFavoriteColorSize) {
+        minFavoriteColorSize = record.getFavoriteColor().length();
+      }
+      if(record.getDateOfBirthAsString().length() > minDateOfBirthSize) {
+        minDateOfBirthSize = record.getDateOfBirthAsString().length();
+      }
+    }
+
+    StringBuilder builder = new StringBuilder();
+
+    addTableCell(builder, lastNameLabel, minLastNameSize);
+    addTableCell(builder, firstNameLabel, minFirstNameSize);
+    addTableCell(builder, emailLabel, minEmailSize);
+    addTableCell(builder, favoriteColorLabel, minFavoriteColorSize);
+    addTableCell(builder, dateOfBirthLabel, minDateOfBirthSize);
+    builder.append(System.lineSeparator());
+
+    // Add 5 to account for the dividing characters between each column in the table
+    int tableWidth = minLastNameSize + minFirstNameSize + minEmailSize + minFavoriteColorSize + minDateOfBirthSize + 5;
+    for (int i = 0; i < tableWidth; i++) {
+      builder.append("-");
+    }
+    builder.append(System.lineSeparator());
+
+    for (Record record : records) {
+      addTableCell(builder, record.getLastName(), minLastNameSize);
+      addTableCell(builder, record.getFirstName(), minFirstNameSize);
+      addTableCell(builder, record.getEmail(), minEmailSize);
+      addTableCell(builder, record.getFavoriteColor(), minFavoriteColorSize);
+      addTableCell(builder, record.getDateOfBirthAsString(), dateOfBirthLabel.length());
+      builder.append(System.lineSeparator());
+    }
+
+    return builder.toString();
+  }
+
+  private static void addTableCell(StringBuilder builder, String str, int minLength) {
+    builder.append(str);
+    padWithSpace(builder, str, minLength);
+    builder.append("|");
+  }
+
+  private static void padWithSpace(StringBuilder builder, String str, int requestedLength) {
+    int difference = requestedLength - str.length();
+    if (difference <= 0) {
+      return;
+    }
+
+    for (int i = 0; i < difference; i++) {
+      builder.append(" ");
+    }
   }
 
 }
